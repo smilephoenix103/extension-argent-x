0ea3fa5c8f3510076dea69312f3ec6658f5b3420
Marios Isaakidis <prometheas@autistici.org>
2023-04-28 21:51:57 +0300

    fix(rpch): extend the Sequencer provider

--- packages/extension/src/shared/network/provider.ts
+++ packages/extension/src/shared/network/provider.ts
@@ -1,17 +1,21 @@
 import * as RPChCrypto from "@rpch/crypto"
 import SDK from "@rpch/sdk"
 import { memoize } from "lodash-es"
-import { RpcProvider, RpcProviderOptions, SequencerProvider } from "starknet"
+import {
+  GatewayError,
+  HttpError,
+  LibraryError,
+  SequencerProvider,
+  buildUrl,
+} from "starknet"
 import { SequencerProvider as SequencerProviderv4 } from "starknet4"
+import { parse, parseAlwaysAsBig, stringify } from "starknet4/dist/utils/json"
 
 import { Network } from "./type"
 
 const getProviderForBaseUrl = memoize((baseUrl: string) => {
   if (baseUrl.endsWith("rpch.tech")) {
-    const rpchOpts: RpcProviderOptions = {
-      nodeUrl: baseUrl,
-    }
-    return new RpchProvider(rpchOpts)
+    return new SequencerRpchProvider({ baseUrl })
   } else {
     return new SequencerProvider({ baseUrl })
   }
@@ -56,15 +60,74 @@ const sdk = new SDK(
   store.get,
 )
 
-class RpchProvider extends RpcProvider {
-  public fetch(method: any, params: any): Promise<any> {
-    return new Promise(() => {
-      // sdk.start();
-      // const provider = "https://alpha4.starknet.io";
-      // const body = JSON.stringify({ method, jsonrpc: "2.0", params, id: 0 });
-      // const req = sdk.createRequest(provider, body);
-      // const res = sdk.sendRequest(req);
-      // sdk.stop();
-    })
+class SequencerRpchProvider extends SequencerProvider {
+  // public fetch(method: any, params: any): Promise<any> {
+  //   return new Promise(() => {
+  //     // sdk.start();
+  //     // const provider = "https://alpha4.starknet.io";
+  //     // const body = JSON.stringify({ method, jsonrpc: "2.0", params, id: 0 });
+  //     // const req = sdk.createRequest(provider, body);
+  //     // const res = sdk.sendRequest(req);
+  //     // sdk.stop();
+  //   })
+  // }
+
+  public async fetch(
+    endpoint: string,
+    options?: {
+      method?: "POST" | "GET"
+      body?: any
+      parseAlwaysAsBigInt?: boolean
+    },
+  ): Promise<any> {
+    const url = buildUrl(this.baseUrl, "", endpoint)
+    const method = options?.method ?? "GET"
+    // @ts-expect-error err
+    const headers = this.getHeaders(method)
+
+    let response: Response
+
+    try {
+      if (method === "GET") {
+        const body = stringify(options?.body)
+        response = await fetch(url, {
+          method,
+          body,
+          headers,
+        })
+      } else {
+        await sdk.start()
+        const rpchReq = await sdk.createRequest(url, stringify(options?.body))
+        // @ts-expect-error err
+        response = await sdk.sendRequest(rpchReq)
+        await sdk.stop()
+      }
+
+      const textResponse = await response.text()
+
+      if (!response.ok) {
+        // This will allow the user to handle contract errors
+        let responseBody: any
+        try {
+          responseBody = parse(textResponse)
+        } catch {
+          throw new HttpError(response.statusText, response.status)
+        }
+        throw new GatewayError(responseBody.message, responseBody.code)
+      }
+
+      const parseChoice = options?.parseAlwaysAsBigInt
+        ? parseAlwaysAsBig
+        : parse
+      return parseChoice(textResponse)
+    } catch (error) {
+      if (error instanceof Error && !(error instanceof LibraryError)) {
+        throw Error(
+          `Could not ${method} from endpoint \`${url}\`: ${error.message}`,
+        )
+      }
+
+      throw error
+    }
   }
 }
