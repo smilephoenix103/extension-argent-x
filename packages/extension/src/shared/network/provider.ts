9ce50847857b7709649cfba266654883424c437e
Marios Isaakidis <prometheas@autistici.org>
2023-04-28 14:37:48 +0300

    feat(rpch): scaffold RPCh provider
    - add "rpch-goerli-alpha" as default network
    - add RpchProvider class that extends starknet.js RpcProvider
    - use RpchProvider when network URL ends with "rpch.tech"

--- packages/extension/src/shared/network/provider.ts
+++ packages/extension/src/shared/network/provider.ts
@@ -1,11 +1,18 @@
 import { memoize } from "lodash-es"
-import { SequencerProvider } from "starknet"
+import { RpcProvider, RpcProviderOptions, SequencerProvider } from "starknet"
 import { SequencerProvider as SequencerProviderv4 } from "starknet4"
 
 import { Network } from "./type"
 
 const getProviderForBaseUrl = memoize((baseUrl: string) => {
-  return new SequencerProvider({ baseUrl })
+  if (baseUrl.endsWith("rpch.tech")) {
+    const rpchOpts: RpcProviderOptions = {
+      nodeUrl: baseUrl,
+    }
+    return new RpchProvider(rpchOpts)
+  } else {
+    return new SequencerProvider({ baseUrl })
+  }
 })
 
 export function getProvider(network: Network) {
@@ -18,4 +25,14 @@ const getProviderV4ForBaseUrl = memoize((baseUrl: string) => {
 
 export function getProviderv4(network: Network) {
   return getProviderV4ForBaseUrl(network.baseUrl)
+}
+
+class RpchProvider extends RpcProvider {
+  public fetch(method: any, params: any): Promise<any> {
+    return fetch(this.nodeUrl, {
+      method: "POST",
+      body: JSON.stringify({ method, jsonrpc: "2.0", params, id: 0 }),
+      headers: this.headers as Record<string, string>,
+    })
+  }
 }
